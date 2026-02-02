d896b62e5e66e066050dd1af93064e76e670e905
WilliamT213 <wtaylor5@hawk.iit.edu>
2026-02-01 21:45:53 -0600

    Assignment Done

--- src/runwait.c
+++ src/runwait.c
@@ -2,11 +2,38 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+
 static void usage(const char *a){fprintf(stderr,"Usage: %s <cmd> [args]\n",a); exit(1);}
 static double d(struct timespec a, struct timespec b){
  return (b.tv_sec-a.tv_sec)+(b.tv_nsec-a.tv_nsec)/1e9;}
-int main(int c,char**v){
-/* TODO : ADD CODE HERE
-*/
- return 0;
+
+ int main(int c,char **v) {
+    if (c < 2) usage(v[0]);
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    pid_t pid = fork();
+    if (pid < 0)
+        DIE("fork");
+
+    if (pid == 0) {
+        execvp(v[1], &v[1]);
+        DIE("execvp");
+    }
+
+    int status;
+    waitpid(pid, &status, 0);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    printf("pid=%d ", pid);
+
+    printf("elapsed=%.3f ", d(start, end));
+
+    if (WIFEXITED(status))
+        printf("exit=%d\n", WEXITSTATUS(status));
+    else if (WIFSIGNALED(status))
+        printf("signal=%d\n", WTERMSIG(status));
+
+    return 0;
 }
